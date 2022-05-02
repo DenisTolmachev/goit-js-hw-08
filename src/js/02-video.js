da453b08424b736ed1656b7ada4d05ebae389165
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-02 18:01:21 +0300

    add player code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,21 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function() {
-   console.log('played the video!');
+player.on('timeupdate', function(data) {
+   localStorage.setItem("videoplayer-current-time", data.seconds);
+   console.log(data.seconds);
 });
+
+const currentPlaybackPosition = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(Number(currentPlaybackPosition)).then(function(seconds) {
+   seconds = number(currentPlaybackPosition);
+}).catch(function(error) {
+   switch (error.name) {
+       case 'RangeError':
+           console.log('Ups:)');
+           break;
+   }
+});
+
+
