a0c5d6955f5c801999f807b6a5e205bc38f48626
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-02 18:49:08 +0300

    add throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function(data) {
+const onLocalStorage = function(data) {
    localStorage.setItem("videoplayer-current-time", data.seconds);
    console.log(data.seconds);
-});
+}
+
+player.on('timeupdate', throttle (onLocalStorage, 1000));
 
 const currentPlaybackPosition = localStorage.getItem("videoplayer-current-time");
 
 player.setCurrentTime(Number(currentPlaybackPosition)).then(function(seconds) {
    seconds = Number(currentPlaybackPosition);
-}).catch(function(error) {
-   switch (error.name) {
-       case 'RangeError':
-           console.log('Ups:)');
-           break;
-   }
 });
 
 
