7199cd562aed6d6e21be17dd63861f80c5134b6f
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-03 21:09:42 +0300

    complete feedback task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const userFeedbackForm = document.querySelector('.feedback-form');
+const FORM_DATA = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(FORM_DATA)) ?? {};
+
+userFeedbackForm.elements.email.value = formData.email ?? '';
+userFeedbackForm.elements.message.value = formData.message ?? '';
+
+const inputHandler = e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FORM_DATA, JSON.stringify(formData));
+  //console.log(formData);
+};
+
+const submitHandler = e => {
+  e.preventDefault();
+  if (
+    userFeedbackForm.elements.email.value == '' ||
+    userFeedbackForm.elements.message.value == ''
+  ) {
+    alert('Заполните все поля');
+    return;
+  }
+  localStorage.removeItem(FORM_DATA);
+  e.currentTarget.reset();
+  console.log(formData);
+};
+
+userFeedbackForm.addEventListener('input', throttle(inputHandler, 500));
+userFeedbackForm.addEventListener('submit', submitHandler);
