97bb118eee0825f0f78f8192d2744c159111f42f
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-04 12:08:51 +0300

    rewrite code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,21 @@
 import throttle from 'lodash.throttle';
 
-const userFeedbackForm = document.querySelector('.feedback-form');
+const userForm = document.querySelector('.feedback-form');
 const FORM_DATA = 'feedback-form-state';
-const formData = JSON.parse(localStorage.getItem(FORM_DATA)) ?? {};
+let formData = {};
 
-userFeedbackForm.elements.email.value = formData.email ?? '';
-userFeedbackForm.elements.message.value = formData.message ?? '';
+console.log(userForm.elements.value);
+
+writeInputData();
 
 const inputHandler = e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(FORM_DATA, JSON.stringify(formData));
-  //console.log(formData);
 };
 
 const submitHandler = e => {
   e.preventDefault();
-  if (
-    userFeedbackForm.elements.email.value == '' ||
-    userFeedbackForm.elements.message.value == ''
-  ) {
+  if (userForm.elements.email.value == '' || userForm.elements.message.value == '') {
     alert('Заполните все поля');
     return;
   }
@@ -27,11 +24,42 @@ const submitHandler = e => {
   console.log(formData);
 };
 
-userFeedbackForm.addEventListener('input', throttle(inputHandler, 500));
-userFeedbackForm.addEventListener('submit', submitHandler);
+function writeInputData() {
+  const savedDataValue = JSON.parse(localStorage.getItem(FORM_DATA));
+  if (savedDataValue) {
+   userForm.elements.value = savedDataValue.value;
+  }
+}
 
-function babelTest() {
-   console.log("babelTest");
-};
+userForm.addEventListener('input', throttle(inputHandler, 500));
+userForm.addEventListener('submit', submitHandler);
 
-babelTest();
+// const userFeedbackForm = document.querySelector('.feedback-form');
+// const FORM_DATA = 'feedback-form-state';
+// const formData = JSON.parse(localStorage.getItem(FORM_DATA)) ?? {};
+
+// userFeedbackForm.elements.email.value = formData.email ?? '';
+// userFeedbackForm.elements.message.value = formData.message ?? '';
+
+// const inputHandler = e => {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem(FORM_DATA, JSON.stringify(formData));
+//   //console.log(formData);
+// };
+
+// const submitHandler = e => {
+//   e.preventDefault();
+//   if (
+//     userFeedbackForm.elements.email.value == '' ||
+//     userFeedbackForm.elements.message.value == ''
+//   ) {
+//     alert('Заполните все поля');
+//     return;
+//   }
+//   localStorage.removeItem(FORM_DATA);
+//   e.currentTarget.reset();
+//   console.log(formData);
+// };
+
+// userFeedbackForm.addEventListener('input', throttle(inputHandler, 500));
+// userFeedbackForm.addEventListener('submit', submitHandler);
